53d3e92c5ea5f74308b4aaca6912f06749212cea
Anirudh Kuppili <anirudhkuppili.dev@gmail.com>
2026-01-12 16:05:10 -0800

    fix(logo): logo stopped lagging now

--- components/landing/Hero.tsx
+++ components/landing/Hero.tsx
@@ -2,16 +2,18 @@
 
 import { useUiStore } from "@/lib/store/useUiStore";
 import Image from "next/image";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Hero() {
   const { logoUrl, logoPictureVersion, setLogoPicture } = useUiStore();
+  const [isReady, setIsReady] = useState(false);
 
   useEffect(() => {
     const fetchLogo = async () => {
       const res = await fetch("/api/assets/logo", { cache: "no-store" });
       const data = await res.json();
       setLogoPicture(data.version, data.url);
+      setIsReady(true);
     };
     fetchLogo();
   }, [setLogoPicture]);
@@ -22,17 +24,18 @@ export default function Hero() {
   const imageSrc = `${baseUrl}?v=${logoPictureVersion}`;
 
   return (
-    <div className="relative grid place-items-center">
-      <div className="w-screen h-[50vh] overflow-none" />{" "}
-      {/* Spacer to reserve space */}
+    <div className="relative w-screen h-[50vh]">
       {/*<RotatingThreeJSModel />*/}
-      <Image
-        src={imageSrc}
-        alt="Logo Image"
-        layout="fill"
-        objectFit="cover"
-        objectPosition="center"
-      />
+      {isReady && (
+        <Image
+          src={imageSrc}
+          alt="Logo Image"
+          fill
+          sizes="100vw"
+          className="object-cover object-center"
+          priority
+        />
+      )}
     </div>
   );
 }
